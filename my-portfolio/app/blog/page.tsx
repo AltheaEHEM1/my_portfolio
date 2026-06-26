b8db8653f635829ec40c0c4cc8165e1f2eedc2e8
Althea Amor J. Asis <altheaasis18@gmail.com>
2026-06-26 16:40:54 +0800

    feat: done hero component that is applied to the pages

--- my-portfolio/app/blog/page.tsx
+++ my-portfolio/app/blog/page.tsx
@@ -1,7 +1,16 @@
-//import React from "react";
+import HeroSection from "@/components/hero/hero";
 
 const Blog = () => {
-	return <div>page</div>;
+	return (
+		<>
+			<HeroSection
+				subtitle="----// 001 - GET TO KNOW ME"
+				title="Blog"
+				description="this is the blog page"
+			/>
+
+		</>
+	);
 };
 
 export default Blog;
--- my-portfolio/app/contact/page.tsx
+++ my-portfolio/app/contact/page.tsx
@@ -1,7 +1,16 @@
-//import React from "react";
+import HeroSection from "@/components/hero/hero";
 
 const Contact = () => {
-	return <div>page</div>;
+	return (
+		<>
+			<HeroSection
+				subtitle="----// 001 - GET TO KNOW ME"
+				title="Projects"
+				description="this is the projects page"
+			/>
+
+		</>
+	);
 };
 
 export default Contact;
